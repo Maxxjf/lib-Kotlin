66b9ad291d9a3963091e96ced4c87a3f5a64fe64
kuzan <kuzan@qi-cloud.com>
2018-03-11 23:06:23 +0800

    新增基于Maderial design 的刷新控件SwipeRefreshLayout
    Signed-off-by: kuzan <kuzan@qi-cloud.com>

---
+++ qclib/src/main/java/com/qcloud/qclib/refresh/swiperefresh/CircleImageView.kt
@@ -0,0 +1,9 @@
+package com.qcloud.qclib.refresh.swiperefresh
+
+/**
+ * Description:
+ * Author: gaobaiqiang
+ * 2018/3/11 下午4:44.
+ */
+class CircleImageView {
+}
---
+++ qclib/src/main/java/com/qcloud/qclib/refresh/swiperefresh/SwipeRefreshLayout.kt
@@ -0,0 +1,9 @@
+package com.qcloud.qclib.refresh.swiperefresh
+
+/**
+ * Description:
+ * Author: gaobaiqiang
+ * 2018/3/11 下午5:07.
+ */
+class SwipeRefreshLayout {
+}
