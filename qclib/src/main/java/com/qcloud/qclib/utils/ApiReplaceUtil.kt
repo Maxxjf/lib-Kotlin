52ea1e2f4ff7982b22e4e112feaeef0f99137b6f
kuzan <kuzan@qi-cloud.com>
2018-03-13 16:43:18 +0800

    优化api过时替换工具类
    Signed-off-by: kuzan <kuzan@qi-cloud.com>

--- qclib/src/main/java/com/qcloud/qclib/utils/ApiReplaceUtil.kt
+++ qclib/src/main/java/com/qcloud/qclib/utils/ApiReplaceUtil.kt
@@ -1,9 +1,68 @@
 package com.qcloud.qclib.utils
 
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.support.annotation.ColorRes
+import android.support.annotation.DrawableRes
+import android.support.annotation.NonNull
+import android.support.v4.content.ContextCompat
+import android.view.View
+
 /**
- * Description:
+ * Description: api过时替换工具类
  * Author: gaobaiqiang
  * 2018/3/13 下午4:24.
  */
-object ContextCompatUtil {
+object ApiReplaceUtil {
+    /**
+     * 获取颜色值
+     *
+     * @param context
+     * @param colorRes 颜色资源id
+     *
+     * @return 颜色值
+     * */
+    fun getColor(@NonNull context: Context, @ColorRes colorRes: Int): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ContextCompat.getColor(context, colorRes)
+        } else {
+            context.resources.getColor(colorRes)
+        }
+    }
+
+    /**
+     * 获取Drawable
+     *
+     * @param context
+     * @param drawableRes 图片资源id
+     *
+     * @return 图片Drawable
+     * */
+    fun getDrawable(@NonNull context: Context, @DrawableRes drawableRes: Int): Drawable? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ContextCompat.getDrawable(context, drawableRes)
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                context.getDrawable(drawableRes)
+            } else {
+                context.resources.getDrawable(drawableRes)
+            }
+        }
+    }
+
+    /**
+     * 给控件设置背景
+     *
+     * @param view 控件
+     * @param drawable 背景
+     * */
+    fun setBackground(@NonNull view: View, drawable: Drawable) {
+        view.setBackgroundResource(0)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            view.background = drawable
+        } else {
+            view.setBackgroundDrawable(drawable)
+        }
+    }
 }
